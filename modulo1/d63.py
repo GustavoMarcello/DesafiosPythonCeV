69f70b99b429046446ed72a04e2c59eb787c4804
Gustavo Marcello <gustavo.g.marcello@gmail.com>
2026-08-17 17:08:57 -0300

    updat with more exercises

--- modulo1/d63.py
+++ modulo1/d63.py
@@ -1,5 +1,5 @@
 """
-EXERCÍCIO D41 - Gerador de Sequência de Fibonacci
+EXERCÍCIO D63 - Gerador de Sequência de Fibonacci
 Crie um programa que:
 1. Peça ao usuário para digitar quantos termos da sequência de Fibonacci deseja
 2. Gere a sequência onde cada número é a soma dos dois anteriores (começando com 0 e 1)
