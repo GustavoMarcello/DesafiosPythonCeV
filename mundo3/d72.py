01c1f4c865035f133a790fc4f3b9683b9e1f592b
GustavoMarcello <gustavo.g.marcello@gmail.com>
2026-08-28 16:30:52 -0300

    update exercises

--- mundo3/d72.py
+++ mundo3/d72.py
@@ -1,7 +1,7 @@
 """
 EXERCÍCIO D72 - Conversor de Número para Extenso
 Crie um programa que:
-1. Peça ao usuário para digitar um número entre 0 e 15
+1. Peça ao usuário para digitar um número entre 0 e 5
 2. Converta o número para extenso em português
 3. Exiba o número por extenso
 
