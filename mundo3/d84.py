1556acc92abe8c92345140497f78fa4e41dffc1f
GustavoMarcello <gustavo.g.marcello@gmail.com>
2026-08-29 16:23:51 -0300

    update

--- mundo3/d84.py
+++ mundo3/d84.py
@@ -2,8 +2,9 @@
 EXERCÍCIO D84 - Lista de pessoas e pesos
 Crie um programa que:
 1. Leia o nome e o peso de várias pessoas, guardando tudo em uma lista
-2. No final, mostre:
+2. Pergunte ao usuário se ele deseja continuar [S/N]
+3. No final, mostre:
     a) Quantas pessoas foram cadastradas
-    b) Uma listagem com as pessoas mais pesadas
-    c) Uma listagem com as pessoas mais leves
+    b) A pessos mais pesada
+    c) A pessoa mais leve
 """
