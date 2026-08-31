f059d16e29c784b81cc93b4f1a1010d64d13d381
GustavoMarcello <gustavo.g.marcello@gmail.com>
2026-08-31 18:00:05 -0300

    update exercies

--- mundo3/d90.py
+++ mundo3/d90.py
@@ -2,5 +2,6 @@
 EXERCÍCIO D90 - Dicionário em Python
 Crie um programa que:
 1. Leia o nome e a média de vários alunos, guardando tudo em um dicionário
-2. No final, mostre o conteúdo da estrutura na tela
+2. Pergunte ao usuário se ele deseja continuar [S/N]
+3. No final, mostre o conteúdo da estrutura na tela
 """
