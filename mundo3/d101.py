6b8ef8bed3e88978d3b0e664e34642d5d1c02324
GustavoMarcello <gustavo.g.marcello@gmail.com>
2026-09-01 17:05:50 -0300

    update

--- mundo3/d101.py
+++ mundo3/d101.py
@@ -3,7 +3,7 @@ EXERCÍCIO D101 - Função para votação
 Crie um programa que:
 1. Contenha uma função que recebeba como parâmetro o ano de nascimento de uma pessoa
 2. Retorne um valor literal indicando se a pessoa tem:
-    - voto NEGADO,
-    - voto OPCIONAL
-    - voto OBRIGATÓRIO
+    - voto NEGADO, (menores de 16 anos)
+    - voto OPCIONAL (entre 16 e 18 anos, maiores de 65 anos)
+    - voto OBRIGATÓRIO (entre 18 e 65 anos)
 """
