2b46ad1d6b6c48770ee877b1416db375affae7d6
GustavoMarcello <gustavo.g.marcello@gmail.com>
2026-09-07 19:30:54 -0300

    update 115

---
+++ mundo3/d115.py
@@ -0,0 +1,10 @@
+"""
+EXERCÍCIO D115 - Sistema de cadastro modularizado
+Crie um programa que:
+1. Permita cadastrar nome e idade de pessoas
+2. Guarde essas informações em um arquivo de texto
+    - Crie o arquivo de texto automaticamente caso não exista
+3. O sistema deverá ter duas opções iniciais:
+    - Cadastrar pessoas
+    - Listar pessoas cadastradas
+"""
